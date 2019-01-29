27562e01882b0560346cb037583e2e1b9948fd2f
oasis <oasisagano@gmail.com>
2019-01-30 01:28:45 +0900

    Added more tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,4 +2,15 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
+  # This test ensures the deck generates/returns 52 cards
+  test "create_deck makes 52 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 52
+  end
+
+  # Ensures that the shuffle methods really shuffles (I know its not 100% safe)
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+  end
 end
